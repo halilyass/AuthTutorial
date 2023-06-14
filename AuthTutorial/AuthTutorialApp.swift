7aac6f71b69bd554323b09ad204c4457efd9d484
Halil YAŞ <106011017+halilyass@users.noreply.github.com>
2023-06-14 10:46:36 +0300

    Initial Commit

---
+++ AuthTutorial/AuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTutorialApp.swift
+//  AuthTutorial
+//
+//  Created by Halil YAŞ on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
